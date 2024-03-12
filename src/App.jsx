58cb3e0129b5bfe07a02750f16a07b25cc672467
Nicoryy <pedronicory@gmail.com>
2024-03-11 23:37:11 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+export default function App() {
+  return (
+    <>
+      <div className="bg-[#292929] grid place-items-center h-100v w-full font-JetBrains text-white font-bold size-5F">
+        <div className="bg-purple-800 grid place-items-center px-10 py-8">
+          <h1>Password Generator</h1>
+
+          <span>password1234567891234567890</span>
+
+          
+        </div>
+      </div>
+    </>
+  );
+}
